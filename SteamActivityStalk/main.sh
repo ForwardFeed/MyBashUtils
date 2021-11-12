a99872627856c4b0013bc58b74098783a91ad373
WhereTheFuckIsMySerotonin <76596109+WhereTheFuckIsMySerotonin@users.noreply.github.com>
2021-11-12 02:15:24 +0100

    uppsX2

--- SteamActivityStalk/main.sh
+++ SteamActivityStalk/main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-APPID="407130"
-USERID="sajt4ever"
+APPID=""
+USERID=""
 URL="https://steamcommunity.com/id/${USERID}/games/?tab=recent"
 logfile="logDiff.log"
 while :
@@ -8,5 +8,5 @@ do
 	date | tr '\n' ' :' >> $logfile
 	curl -s "${URL}" | grep -Eo "appid\":${APPID}[^{]+" | grep -Po "hours\":\"\K[0-9.]+|hours_forever\":\"\K[0-9.]+" | tr '\n' ' ' >> ${logfile}
 	echo >> $logfile
-	sleep 2
+	sleep 600
 done
